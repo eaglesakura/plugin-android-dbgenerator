671ae700662f2199c5a977907c29c2e22d28ede4
eaglesakura <eagle.sakura@gmail.com>
2016-07-26 15:28:26 +0900

    plugin名を変更

---
+++ src/main/java/com/eaglesakura/android/property/model/DaoSource.java
@@ -0,0 +1,21 @@
+package com.eaglesakura.android.property.model;
+
+/**
+ * GreenDAOの生成ファイル
+ */
+public class DaoSource {
+
+    /**
+     * 生成元のパッケージ名
+     */
+    public String basePackageName;
+
+    /**
+     * 1つのSchemaを定義する
+     *
+     * 1Schemaにつき1DaoMaster/DaoSessionが生成される
+     */
+    public static class Schema {
+        public String packageName;
+    }
+}
