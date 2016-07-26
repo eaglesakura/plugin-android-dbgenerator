39dd0adc4abe35189073ba8f4092f11f605767d7
eaglesakura <eagle.sakura@gmail.com>
2016-07-26 15:50:01 +0900

    プラグイン名を変更

--- src/main/java/com/eaglesakura/android/property/model/DaoSource.java
+++ src/main/java/com/eaglesakura/android/property/model/DaoSource.java
@@ -1,5 +1,8 @@
 package com.eaglesakura.android.property.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * GreenDAOの生成ファイル
  */
@@ -16,6 +19,46 @@ public class DaoSource {
      * 1Schemaにつき1DaoMaster/DaoSessionが生成される
      */
     public static class Schema {
+        /**
+         * schemaのパッケージ名
+         */
         public String packageName;
+
+        /**
+         * schema version
+         */
+        public int version;
+    }
+
+    public static class Entity {
+        public String name;
+    }
+
+    /**
+     * カラムデータ
+     */
+    public static class Column {
+
+    }
+
+    public static class Property {
+        /**
+         * プロパティ名
+         */
+        public String name;
+
+        /**
+         * 型
+         * `int`, `String`, `id`...
+         */
+        public String type;
+
+        /**
+         * `unique`
+         * `notNull`
+         * `primaryKey`
+         * `index`
+         */
+        public List<String> options = new ArrayList<>();
     }
 }
